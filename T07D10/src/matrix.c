724cbc9d858316a184fccb84d46e03c6ab8c8168
vnovigr <vivpost@mail.ru>
2023-08-11 18:45:30 +0300

    T07D10 quest 3 done

--- T07D10/src/matrix.c
+++ T07D10/src/matrix.c
@@ -4,12 +4,12 @@
 #define ROWS_MAX 100
 #define COLUMNS_MAX 100
 
+void output(int **a, int r, int c);
+
 int main(void) {
     int rows = 0, columns = 0;
     int move;
-    int count = 0;
     int matrix1[ROWS_MAX][COLUMNS_MAX];
-    // int **pointer_array = NULL;
 
     scanf("%d", &move);
     switch (move) {
@@ -41,90 +41,112 @@ int main(void) {
 
             break;
         case 2:
-            // if (scanf("%d %d", &rows, &columns) != 2 || rows != (int)rows || columns != (int)columns ||
-            // rows < 1 || columns < 1) {
-            // printf("n/a");
-            // break;
-            // }
-            scanf("%d %d", &rows, &columns);
-            int **singl_array_matrix = malloc(rows * columns * sizeof(int) + rows * sizeof(int *));
-            int *ptr = (int *)(singl_array_matrix + rows);
-            count = 0;
+            if (scanf("%d %d", &rows, &columns) != 2 || rows != (int)rows || columns != (int)columns ||
+                rows < 1 || columns < 1 || getchar() != '\n') {
+                printf("n/a");
+                break;
+            }
+            int **matrix2 = malloc(rows * columns * sizeof(int) + rows * sizeof(int *));
+            int *ptr_matrix2 = (int *)(matrix2 + rows);
 
             for (int i = 0; i < rows; i++) {
-                singl_array_matrix[i] = ptr + columns * i;
+                matrix2[i] = ptr_matrix2 + columns * i;
             }
 
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    singl_array_matrix[i][j] = ++count;
+                    int temp;
+                    if (scanf("%d", &temp) != 1 || temp != (int)temp) {
+                        printf("n/a");
+                        free(matrix2);
+                        return 1;
+                    } else {
+                        matrix2[i][j] = temp;
+                    }
                 }
             }
 
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    printf("%d ", singl_array_matrix[i][j]);
-                }
-                printf("\n");
-            }
-            free(singl_array_matrix);
+            output(matrix2, rows, columns);
+            free(matrix2);
+
             break;
         case 3:
-            scanf("%d %d", &rows, &columns);
-            int **pointer_array = (int **)malloc(rows * sizeof(int *));
-            int *values_array = (int *)malloc(rows * columns * sizeof(int));
-            count = 0;
+            if (scanf("%d %d", &rows, &columns) != 2 || rows != (int)rows || columns != (int)columns ||
+                rows < 1 || columns < 1 || getchar() != '\n') {
+                printf("n/a");
+                break;
+            }
 
+            int **matrix3 = malloc(rows * sizeof(int *));
             for (int i = 0; i < rows; i++) {
-                pointer_array[i] = values_array + columns * i;
+                matrix3[i] = malloc(columns * sizeof(int));
             }
 
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    pointer_array[i][j] = ++count;
+                    int temp;
+                    if (scanf("%d", &temp) != 1 || temp != (int)temp) {
+                        printf("n/a");
+                        free(matrix3);
+                        return 1;
+                        // break;
+                    } else {
+                        matrix3[i][j] = temp;
+                    }
                 }
             }
+
+            output(matrix3, rows, columns);
 
             for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    printf("%d ", pointer_array[i][j]);
-                }
-                printf("\n");
+                free(matrix3[i]);
             }
-
-            free(values_array);
-            free(pointer_array);
+            free(matrix3);
             break;
         case 4:
-            scanf("%d %d", &rows, &columns);
-            count = 0;
-            int **pointer_array = (int **)malloc(rows * sizeof(int *));
+            if (scanf("%d %d", &rows, &columns) != 2 || rows != (int)rows || columns != (int)columns ||
+                rows < 1 || columns < 1) {
+                printf("n/a");
+                break;
+            }
+
+            int **matrix4 = malloc(rows * sizeof(int *));
+            int *ptr_matrix4 = malloc(rows * columns * sizeof(int));
 
             for (int i = 0; i < rows; i++) {
-                pointer_array[i] = malloc(columns * sizeof(int));
+                matrix4[i] = ptr_matrix4 + columns * i;
             }
 
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    pointer_array[i][j] = ++count;
+                    int temp;
+                    if (scanf("%d", &temp) != 1 || temp != (int)temp) {
+                        printf("n/a");
+                        free(matrix4);
+                        free(ptr_matrix4);
+                        return 1;
+                    } else {
+                        matrix4[i][j] = temp;
+                    }
                 }
             }
 
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    printf("%d ", pointer_array[i][j]);
-                }
-                printf("\n");
-            }
+            output(matrix4, rows, columns);
+            free(ptr_matrix4);
+            free(matrix4);
 
-            for (int i = 0; i < rows; i++) {
-                free(pointer_array[i]);
-            }
-
-            free(pointer_array);
             break;
         default:
             printf("n/a");
             break;
+    }
+}
+
+void output(int **a, int r, int c) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
     }
 }
